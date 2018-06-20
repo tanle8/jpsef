26560089d7ae19dcf7b317857fe2c9c610c3f41f
tanld <tanld.ce@gmail.com>
2018-06-20 11:31:19 +0700

    hottestInManyDays + test code

--- week3/week3-src/CSVMax/CSVMax.java
+++ week3/week3-src/CSVMax/CSVMax.java
@@ -43,8 +43,45 @@ public class CSVMax
 
     public void testHottestInDay()
     {
-        FileResource fr = new FileResource("data/2015/weather-2015-01-01.csv");
+        FileResource fr = new FileResource("../../week3-data/nc_weather/2015/weather-2015-01-01.csv");
         CSVRecord largest = hottestHourseInFile(fr.getCSVParser());
-        System.out.println("hottest temperature was " + largest.get("TemperatureF") + " at " + largest.get("TimerEST"));
+        System.out.println("hottest temperature was " + largest.get("TemperatureF") + " at " + largest.get("TimeEST"));
+    }
+    
+    public CSVRecord hottestInManyDays()
+    {
+        CSVRecord largestSoFar = null;
+        DirectoryResource dr = new DirectoryResource();
+        // iterate over files
+        for (File f : dr.selectedFiles())
+        {
+            // Now we have this file, create new file resource
+            FileResource fr = new FileResource(f);
+            // use method to get largest in file
+            CSVRecord currentRow = hottestHourseInFile(fr.getCSVParser());
+            if (largestSoFar == null)
+            {
+                largestSoFar = currentRow;
+            }
+            else
+            {
+                double currentTemp = Double.parseDouble(currentRow.get("TemperatureF"));
+                double largestTemp = Double.parseDouble(largestSoFar.get("TemperatureF"));
+                // Check if currentRow's temperature > largestSoFar
+                if (currentTemp > largestTemp)
+                {
+                    // If so update largestSoFar to currentRow
+                    largestSoFar = currentRow;
+                }
+            }
+        }
+        // The largestSoFar is the answer
+        return largestSoFar;
+    }
+    
+    public void testHottestInManyDays()
+    {
+        CSVRecord largest = hottestInManyDays();
+        System.out.println("hottest temperature was " + largest.get("TemperatureF") + " at " + largest.get("DateUTC"));
     }
 }
