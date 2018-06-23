3acb681dfa0c3cc409637ea164ebf1e06e66ce4e
tanld <tanld.ce@gmail.com>
2018-06-23 17:05:58 +0700

    Finish BabyName class

--- week4/week4-src/w4a1-parsingBirthsData/ParsingBirthsData.java
+++ week4/week4-src/w4a1-parsingBirthsData/ParsingBirthsData.java
@@ -7,6 +7,8 @@
  */
 import edu.duke.FileResource;
 import org.apache.commons.csv.*;
+import edu.duke.DirectoryResource;
+import java.io.File;
 
 public class ParsingBirthsData {
     public void totalBirths(FileResource fr)
@@ -48,5 +50,91 @@ public class ParsingBirthsData {
         totalBirths(fr);
     }
 
-         
+    public long getRank(String name, int year, String gender)
+    {
+        long rank = -1;
+        
+        DirectoryResource dr = new DirectoryResource();
+        for(File f : dr.selectedFiles())
+        {
+            if (f.getName().contains(Integer.toString(year)))
+            {
+                FileResource fr = new FileResource(f);
+                CSVParser parser = fr.getCSVParser(false);    // "false" - dont has header row
+                for (CSVRecord currentRow : parser)
+                {
+                    String currentRowName = currentRow.get(0);
+                    String currentRowGender = currentRow.get(1);
+                    
+                    if (currentRowGender.equals(gender) && currentRowName.equals(name))
+                    {
+                        rank = currentRow.getRecordNumber();
+                    }
+                }
+                return rank;
+            }
+        }
+        
+        return rank;
+    }
+    
+    public String getName(int year, long rank, String gender)
+    {
+        String name = "";
+        FileResource fr = new FileResource();
+        CSVParser parser = fr.getCSVParser(false);
+
+        for (CSVRecord currentRow : parser)
+        {
+            long currentRowRank = currentRow.getRecordNumber();
+            String currentRowGender = currentRow.get(1);
+            String currentRowName = currentRow.get(0);
+            
+            if (currentRowRank == rank && currentRowGender.equals(gender))
+            {
+                name = currentRowName;
+            }
+        }
+        
+        if (name == "")
+        {
+            return "NO NAME";
+        }
+        
+        return name;
+    }
+    
+    public void whatIsNameInYear(String name, int year, int newYear, String gender)
+    {
+        FileResource fr = new FileResource();
+        FileResource frNew = new FileResource();
+        CSVParser parser = fr.getCSVParser(false);
+        CSVParser parserNew = frNew.getCSVParser(false);
+        String nameNew = "";
+        long popularity = 0;
+        
+        for(CSVRecord currentRow : parser)
+        {
+            String currentRowName = currentRow.get(0);
+            String currentRowGender = currentRow.get(1);
+            
+            if(currentRowName.equals(name) && currentRowGender.equals(gender))
+            {
+                popularity = currentRow.getRecordNumber();
+            }
+        }
+        
+        for(CSVRecord currentRow : parserNew)
+        {
+            String currentRowGender = currentRow.get(1);
+            long currentRowPopularity = currentRow.getRecordNumber();
+            
+            if(currentRowGender.equals(gender) && (currentRowPopularity == popularity))
+            {
+                nameNew = currentRow.get(0);
+            }
+        }
+        
+        System.out.println(name + " born in " + year + " would be " + nameNew + " if she was born in " + newYear);
+    }
 }
