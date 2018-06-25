fb56a934010226a4a56eb7eb2fb1e7c5fa84ca36
tanld <tanld.ce@gmail.com>
2018-06-25 11:59:13 +0700

    minor changes before pulling from master

--- week4/week4-src/w4a1-grayScaleCvt/GrayScaleConverter.java
+++ week4/week4-src/w4a1-grayScaleCvt/GrayScaleConverter.java
@@ -15,23 +15,23 @@ public class GrayScaleConverter
     {
         //I made a blank image of the same size
         ImageResource outImage = new ImageResource(inImage.getWidth(), inImage.getHeight());
-	//for each pixel in outImage
-	for (Pixel pixel : outImage.pixels())
-	{
-	    //look at the corresponding pixel in inImage
-	    Pixel inPixel = inImage.getPixel(pixel.getX(), pixel.getY());
-	    //compute inPixel's red + inPixel's blue + inPixel's green
-	    //divide that sum by 3 (call it average)
-	    int average = (inPixel.getRed() + inPixel.getBlue() + inPixel.getGreen()) / 3;
-	    //set pixel's red to average
-	    pixel.setRed(average);
-	    //set pixel's green to average
-	    pixel.setGreen(average);
-	    //set pixel's blue to average
-	    pixel.setBlue(average);
-        }
-	//outImage is your answer
-	return outImage;
+		//for each pixel in outImage
+		for (Pixel pixel : outImage.pixels())
+		{
+	    	//look at the corresponding pixel in inImage
+	    	Pixel inPixel = inImage.getPixel(pixel.getX(), pixel.getY());
+	    	//compute inPixel's red + inPixel's blue + inPixel's green
+	    	//divide that sum by 3 (call it average)
+	    	int average = (inPixel.getRed() + inPixel.getBlue() + inPixel.getGreen()) / 3;
+	    	//set pixel's red to average
+	    	pixel.setRed(average);
+	    	//set pixel's green to average
+	    	pixel.setGreen(average);
+	    	//set pixel's blue to average
+	    	pixel.setBlue(average);
+		}
+		//outImage is your answer
+		return outImage;
     }
 
     public void testMakeGray()
