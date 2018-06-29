53f0a43848252ba374abddb01537fd9562223f9f
tanld <tanld.ce@gmail.com>
2018-06-29 13:45:54 +0700

    Add ability to  encrypt message with two different keys (Finish Caesar Cipher)

--- 3-jpalsd/week1/a2/CaesarCipher.java
+++ 3-jpalsd/week1/a2/CaesarCipher.java
@@ -62,5 +62,75 @@ public class CaesarCipher
         String decrypted = encrypt(encrypted, 26-key);
         System.out.println(decrypted);
     }
-
+    
+    // Refactor code
+    public char encryptChar(char input, int key)
+    {
+        // if character lowercase
+        if(Character.isLowerCase(input))
+        {
+            String alphabetLowerCase = "abcdefghijklmnopqrstuvwxyz";
+            String shiftedAlphabet = alphabetLowerCase.substring(key) + alphabetLowerCase.substring(0, key);
+            int currentIdx = alphabetLowerCase.indexOf(input);
+            char encrypted = shiftedAlphabet.charAt(currentIdx);
+            
+            return encrypted;
+        }
+        // if character uppercase
+        if(Character.isUpperCase(input))
+        {
+            String alphabetUpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            String shiftedAlphabet = alphabetUpperCase.substring(key) + alphabetUpperCase.substring(0, key);
+            int currentIdx = alphabetUpperCase.indexOf(input);
+            char encrypted = shiftedAlphabet.charAt(currentIdx);
+            
+            return encrypted;            
+        }
+        
+        return '\u0000';    // null character
+    }
+    
+    public String encryptTwoKeys(String input, int key1, int key2)
+    {
+        // Make a StringBuilder with message
+        StringBuilder encrypted = new StringBuilder(input);
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        // Loop through each character in the input string
+        for (int i = 0; i < encrypted.length(); i++)
+        {
+            char currentChar = input.charAt(i);
+            int currentCharIdx = alphabet.indexOf(Character.toUpperCase(currentChar));  // Find the index of the messageChar in the alphabet (newChar)
+            if (currentCharIdx != -1)    // If messageChar is in the alphabet, not a space/punctuation
+            {
+                if (i % 2 == 0)
+                {
+                    char encrtdChar = encryptChar(currentChar, key1);
+                    encrypted.setCharAt(i, encrtdChar);
+                }
+                else
+                {
+                    char encrtdChar = encryptChar(currentChar, key2);
+                    encrypted.setCharAt(i, encrtdChar);
+                }
+            }
+            // Otherwise do nothing
+        }
+        
+        return encrypted.toString();
+    }
+    
+    public void testEncryptTwoKeys()
+    {
+        String result = encryptTwoKeys("First Legion", 23, 17);
+        String testCase1 = "Czojq Ivdzle";
+        
+        if(result.equals(testCase1))
+        {
+            System.out.println("Test case 1: pass \n\t"+ result + "\n\t" + testCase1);
+        }
+        else
+        {
+            System.out.println("Test case 1: failed. It's failed! \n\t" + result + "\n\t" + testCase1);
+        }
+    }
 }
