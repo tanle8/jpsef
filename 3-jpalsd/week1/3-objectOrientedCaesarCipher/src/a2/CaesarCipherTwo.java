6f81486321ed77483a571f1c5978c5bfa48d7a46
tanld <tanld.ce@gmail.com>
2018-07-07 21:28:09 +0700

    CaesarCipher with two keys initialization

---
+++ 3-jpalsd/week1/3-objectOrientedCaesarCipher/src/a2/CaesarCipherTwo.java
@@ -0,0 +1,21 @@
+
+/**
+ * Put together the CaesarCipherTwo class that
+ * encrypts a message with two keys
+ * (the same way as the previous lesson)
+ * 
+ * @author (Tan Le) 
+ * @version (07-07-2018)
+ */
+public class CaesarCipherTwo {
+    // Create the class's private fields
+    private String alphabet;
+    private String shiftedAlphabet1;
+    private String shiftedAlphabet2;
+    
+    // Create the constructor
+    public CaesarCipherTwo(int key1, int key2)
+    {
+        
+    }
+}
---
+++ 3-jpalsd/week1/3-objectOrientedCaesarCipher/src/a2/TestCaesarCipher.java
@@ -0,0 +1,10 @@
+
+/**
+ * Write a description of TestCaesarCipher here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class TestCaesarCipher {
+
+}
